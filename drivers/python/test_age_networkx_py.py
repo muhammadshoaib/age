f2dec5ff19b5655e984f4738f78a5c74e7769d83
Tito Osadebey <94079649+titoausten@users.noreply.github.com>
2023-05-30 18:42:44 +0100

    Update and rename test_nx_age_py.py to test_age_networkx_py.py
    - Wrote unit tests for Moontasir's code
    - Both functions: ageToNetworkx() and networkxToAge() passed the test.

--- drivers/python/test_age_networkx_py.py
+++ drivers/python/test_age_networkx_py.py
@@ -1,26 +1,45 @@
-from age.models import Vertex
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import age
+from age.models import *
 import unittest
 import decimal
-from age import *
-from nx_age import *
+from age_networkx import *
 import networkx as nx
 
-DSN = "host=localhost port=5432 dbname=tee_demodb user=tito password=jacz"
+DSN = "host=172.17.0.2 port=5432 dbname=postgres user=postgres password=agens"
 TEST_GRAPH_NAME = "test"
 
-class TestAgeBasic(unittest.TestCase):
+class TestSupportNetworkx(unittest.TestCase):
     ag = None
     def setUp(self):
+        print("\n")
         print("Connecting to Test Graph.....")
-        self.ag = age.connect(graph=TEST_GRAPH_NAME, dsn=DSN)
+        # Setting up connection to work with Graph
+        self.ag = age.Age().connect(TEST_GRAPH_NAME, dsn=DSN)
 
     def tearDown(self):
         # Clear test data
         print("Deleting Test Graph.....")
         age.deleteGraph(self.ag.connection, self.ag.graphName)
         self.ag.close()
+        
 
-    def testload_to_networkx(self):
+    def testageToNetworkx(self):
+        print("[Test AGE to NetworkX]")
         ag = self.ag
         # Create nodes
         ag.execCypher("CREATE (n:Person {name: %s}) ", params=('Jack',))
@@ -36,34 +55,40 @@ class TestAgeBasic(unittest.TestCase):
                     CREATE p=((a)-[r:workWith {weight: 10}]->(b)) """, params=('Jack', 'Smith',))
         ag.commit()
 
-        G = load_to_networkx(graph_name=TEST_GRAPH_NAME, DSN=DSN, query_output=None)
+        G = ageToNetworkx(self.ag.connection, TEST_GRAPH_NAME)
         
-        # Check that the graph has the expected properties
+        # Check that the G has the expected properties
         self.assertIsInstance(G, nx.DiGraph)
-        # Check that the graph has the correct number of nodes and edges
+        # Check that the G has the correct number of nodes and edges
         self.assertEqual(len(G.nodes), 3)
         self.assertEqual(len(G.edges), 2)
         # Check that the node properties are correct
         for node in G.nodes:
+            self.assertEqual(int, type(node))
             self.assertEqual(G.nodes[node]['label'], 'Person')
             self.assertIn('name', G.nodes[node]['properties'])
+            self.assertIn('properties', G.nodes[node])
             self.assertEqual(str, type(G.nodes[node]['label']))
 
         # Check that the edge properties are correct
         for edge in G.edges:
+            self.assertEqual(tuple, type(edge))
+            self.assertEqual(int, type(edge[0]) and type(edge[1]))
             self.assertEqual(G.edges[edge]['label'], 'workWith')
             self.assertIn('weight', G.edges[edge]['properties'])
             self.assertEqual(int, type(G.edges[edge]['properties']['weight']))
+            
 
-    def testload_from_networkx(self):
-        graph = nx.DiGraph()
-        graph.add_node(1, label='Person', properties={'name': 'Tito', 'age': '26', 'id': 1})
-        graph.add_node(2, label='Person', properties={'name': 'Austen', 'age': '26', 'id': 2})
-        graph.add_edge(1, 2, label='KNOWS', properties={'since': '1997', 'start_id': 1, 'end_id': 2})
+    def testnetworkxToAge(self):
+        print("[Test NetworkX to AGE]")
+        ag = self.ag
+        # Create NetworkX graph
+        G = nx.DiGraph()
+        G.add_node(1, label='Person', properties={'name': 'Tito', 'age': '26', 'id': 1})
+        G.add_node(2, label='Person', properties={'name': 'Austen', 'age': '26', 'id': 2})
+        G.add_edge(1, 2, label='KNOWS', properties={'since': '1997', 'start_id': 1, 'end_id': 2})
 
-        load_from_networkx(G=graph, graph_name='test_graph', DSN=DSN)
-
-        ag = age.connect(graph='test_graph', dsn=DSN)
+        networkxToAge(self.ag.connection, G, TEST_GRAPH_NAME)
 
         # Check that node(s) were created
         cursor = ag.execCypher('MATCH (n) RETURN n')
@@ -71,8 +96,8 @@ class TestAgeBasic(unittest.TestCase):
         # Check number of vertices created
         self.assertEqual(len(result), 2)
         # Checks if type of property in query output is a Vertex
-        self.assertEqual(age.models.Vertex, type(result[0][0]))
-        self.assertEqual(age.models.Vertex, type(result[1][0]))
+        self.assertEqual(Vertex, type(result[0][0]))
+        self.assertEqual(Vertex, type(result[1][0]))
 
         # Check that edge(s) was created
         cursor = ag.execCypher('MATCH ()-[e]->() RETURN e')
@@ -80,7 +105,7 @@ class TestAgeBasic(unittest.TestCase):
         # Check number of edge(s) created
         self.assertEqual(len(result), 1)
         # Checks if type of property in query output is an Edge
-        self.assertEqual(age.models.Edge, type(result[0][0]))
-
+        self.assertEqual(Edge, type(result[0][0]))
+    
 if __name__ == '__main__':
     unittest.main()
